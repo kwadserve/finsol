71f84a7356554b2295c0303e981b6de54dbdeb08
Nayan7030 <np.phpdeveloper@gmail.com>
2023-09-13 23:15:27 +0530

    added the payment status column in admin dashboard

--- resources/views/admin/pages/users/forms/trust_details.blade.php
+++ resources/views/admin/pages/users/forms/trust_details.blade.php
@@ -12,6 +12,7 @@
                             <th scope="col">Admin Note</th>
                             <th scope="col">User Note</th>
                             <th scope="col">Details</th>
+                            <th scope="col">Payment Status</th>
                             <th scope="col">Status</th>
                             <th scope="col">Note/Approve</th>
                         </tr>
@@ -27,6 +28,13 @@
                             <td>{{($detail->admin_note)?$detail->admin_note:'--'}}</td>
                             <td>{{($detail->user_note)?$detail->user_note:'--'}}</td>
                             <td><a href="{{ url('admin/user/forms/details/trust/'.$detail->id) }}">Details</a></td>
+                            <td>
+                                                    @if($detail->payment_status == 'Credit')
+                                                    <span class="btn btn-success ml-1 mb-1 btn-sm"> Paid</span>
+                                                    @else
+                                                    <span class="btn btn-warning ml-1 mb-1 btn-sm"> Pending</span>
+                                                    @endif
+                                                    </td>
                             <td>
                                 @if($detail->status == 2)
                                 <div><span
