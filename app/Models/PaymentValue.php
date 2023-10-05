52aacebd09eff668055f088b8c24eedefdb9425d
Rakesh Ranjan Jena <rakeshranjanjena7@gmail.com>
2023-10-05 10:06:38 +0530

    Admin Sidebar and header updated

---
+++ app/Models/PaymentValue.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class PaymentValue extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'payment_values';
+
+    protected $fillable = [
+        'form_type',
+        'value',
+    ];
+}
